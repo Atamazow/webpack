13463ceb4f3367c73d149051d8ccd550df61b468
Насырбек <64436023+Atamazow@users.noreply.github.com>
2023-03-23 12:05:39 +0300

    Настроил css modules

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Counter} from "./components/Counter";
+import './index.scss'
+function App() {
+    return (
+        <div className='app'>
+            asdas
+            <Counter/>
+        </div>
+    );
+}
+
+export default App;
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './Counter.scss'
+import classes from './Counter.module.scss'
 export function Counter() {
   const [count, setCount] = useState(0);
   const handleClick = () => {
@@ -7,7 +7,7 @@ export function Counter() {
   };
 
   return (
-    <div>
+    <div className={classes.btn}>
       {count}
       <button onClick={handleClick}>Plus</button>
     </div>
