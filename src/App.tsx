ef1f4b0a29d8ab5e98a515394bd6def4413a5bc9
Насырбек <64436023+Atamazow@users.noreply.github.com>
2023-03-24 22:47:52 +0300

    Сделал Тему для фона с помощью useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React, { Suspense } from 'react';
-import {Route, Routes, Link} from "react-router-dom";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import './index.scss'
-
-
+import React, { Suspense, useContext, useState } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import "./styles/index.scss";
+import { Theme, ThemeContext } from "./theme/ThemeContext";
+import UseTheme from "./theme/useTheme";
 
 function App() {
-    return (
-        <div className='app'>
-            <Link to={'/'}>Главная страница</Link>
-            <Link to={'/about'}>О нас</Link>
-           <Suspense fallback={<div>...Loading</div>}>
-               <Routes>
-                   <Route path='/about' element={<AboutPageAsync/>}/>
-                   <Route path='/' element={<MainPageAsync/>}/>
-               </Routes>
-           </Suspense>
-
-        </div>
-    );
+    const {theme, toggleTheme} = UseTheme()
+  return (
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}> Toggle</button>
+      <br />
+      <Link to={"/"}>Главная страница</Link>
+      <Link to={"/about"}>О нас</Link>
+      <Suspense fallback={<div>...Loading</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
