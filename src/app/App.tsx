60c731de9eeeea8533c8050418fcf04c8c708368
Насырбек <64436023+Atamazow@users.noreply.github.com>
2023-03-27 02:48:18 +0300

    Сделал NavBar шамблоны для разработки UI KIT компонент

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,19 @@
-import React, { Suspense} from "react";
-import { Route, Routes, Link } from "react-router-dom";
+import React from "react";
 import {classNames} from "shared/lib/classNames/classNames";
 import { UseTheme } from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
 import "./styles/index.scss";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 
 function App() {
     const {theme, toggleTheme} = UseTheme()
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}> Toggle</button>
-      <br />
-      <Link to={"/"}>Главная страница</Link>
-      <Link to={"/about"}>О нас</Link>
+        <Navbar/>
         <AppRouter/>
+        <button onClick={toggleTheme}> Toggle</button>
+
     </div>
   );
 }
